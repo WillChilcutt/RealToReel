8ef1f184ed73dcc623d2a36ee80da1efd3910172
Will Chilcutt <will@synaptian.com>
2012-05-20 13:20:52 -0400

    first commit

---
+++ RealToReel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RealToReel
+//
+//  Created by Will Chilcutt on 5/20/12.
+//  Copyright (c) 2012 ETSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
